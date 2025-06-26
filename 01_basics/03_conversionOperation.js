bd78451a3e555eb69b0d5f200e33de04a4c2a0b3
Abhishek <abhishek.edu0721@gmail.com>
2025-06-27 00:01:46 +0530

    03 file

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,26 @@
+let score = "33";
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+
+let isLoggedIn = 1
+
+let booleanisLoggedIn = Boolean(isLoggedIn);
+
+// console.log( booleanisLoggedIn); 
+
+// 1 => true, 0 => false, " "=> false, "string"=> true, 
+
+
+
+let someNumber = 33;
+
+let stringsomeNumber = String(someNumber);
+
+console.log( stringsomeNumber);
+console.log(typeof stringsomeNumber);
+
+
