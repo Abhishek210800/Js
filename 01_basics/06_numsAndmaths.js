8c20b2aeb4f316d9e9904d497c72fe95b2c50823
Abhishek <abhishek.edu0721@gmail.com>
2025-06-28 22:46:57 +0530

    nums&math

---
+++ 01_basics/06_numsAndmaths.js
@@ -0,0 +1,55 @@
+const score = 100;
+// console.log(score);
+
+
+const balance = new Number(100);
+// console.log(balance);
+
+// console.log(balance.toString());
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+
+
+const newNum = 23.9999;
+
+// console.log(newNum.toPrecision(3));
+
+const newNum2 = 23.0129;
+// console.log(newNum2.toPrecision(3));
+
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//*******************Maths in Js**************
+
+//this chanegs the negative values to the postive one....
+// console.log(Math.abs(-9));
+
+// console.log(Math.round(7.62));
+
+//THIS CHANEGS THE VALUE TO 5..even if the value is little greater then 4
+// console.log(Math.ceil(4.12));
+
+//it takes the lowest value possible
+// console.log(Math.floor(4.92));
+
+
+// console.log(Math.min(4,4,57,2,6,7,1,));
+// console.log(Math.max(4,4,57,2,6,7,1,));
+
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+
+const min =10;
+const max =20;
+
+console.log(Math.floor(Math.random()*(max-min +1))+min);
+
+
+
