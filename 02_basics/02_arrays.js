3f04aa3fc65dfc1878cb6a1b122e52df0f4d3b4d
Abhishek <abhishek.edu0721@gmail.com>
2025-06-29 01:20:33 +0530

    more arrays methods

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,61 @@
+const marvel_heros = ["thor", "ironman", "spiderman"]
+
+const dc_heroes = ["flash", "batman","superman"]
+
+const indHeroes = ["Krishna", "Bajrangbali", "Mahadev"]
+
+// marvel_heros.push(dc_heroes);
+// console.log(marvel_heros);
+// console.log(marvel_heros[3][1]);
+//    above code output
+
+/*[ 'thor', 'ironman', 'spiderman', [ 'flash', 'batman', 'superman' ] ]
+ batman*/
+
+
+//  const newHeroes = marvel_heros.concat(dc_heroes);
+//  console.log(newHeroes);
+
+ //output for line 14 and 15 ...[ 'thor', 'ironman', 'spiderman', 'flash', 'batman', 'superman' ]
+ 
+
+//  const newHeroes = [...marvel_heros, ...dc_heroes]
+//  console.log(newHeroes);
+ //This also works same as the concat  
+ // But here we can add multiple arrays[ 'thor', 'ironman', 'spiderman', 'flash', 'batman', 'superman' ]
+ 
+//  const newHeroes = [...marvel_heros, ...dc_heroes, ...indHeroes]
+//  console.log(newHeroes);
+ /*[
+  'thor',
+  'ironman',
+  'spiderman',
+  'flash',
+  'batman',
+  'superman',
+  'Krishna',
+  'Bajrangbali',
+  'Mahadev'
+] */
+
+
+// const anotherArray = [1,2,3,[4,5,6],[32,4,5,[33,23,12]]]
+// const real_another_array = anotherArray.flat(Infinity)
+// console.log(real_another_array);
+
+//  above output [
+//    1,  2, 3, 4,  5,
+//    6, 32, 4, 5, 33,
+//   23, 12
+// ]
+
+
+console.log(Array.isArray("Abhishek"));
+console.log(Array.from("Abhishek"));
+/*false
+[   
+  'A', 'b', 'h',
+  'i', 's', 'h',
+  'e', 'k'
+]*/
+
